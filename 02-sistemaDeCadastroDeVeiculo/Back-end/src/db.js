d16432cbce877ae70926364b7ec7cb4352636276
Joao Pedro <joaopedrodiasde@gmail.com>
2025-05-30 15:16:42 -0300

    adicionando

--- 02-sistemaDeCadastroDeVeiculo/Back-end/src/db.js
+++ 02-sistemaDeCadastroDeVeiculo/Back-end/src/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres.xradffjfmtjwlmgwvkjm:052002Joao@aws-0-sa-east-1.pooler.supabase.com:5432/postgres',
+  connectionString: 'postgresql://postgres.xradffjfmtjwlmgwvkjm:052002Joao@@aws-0-sa-east-1.pooler.supabase.com:5432/postgres',
   ssl: { rejectUnauthorized: false }
 });
 
